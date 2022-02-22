a8d0f91f9af294f0a5ed703623fffe736999e4c2
David Thole <david@thedarktrumpet.com>
2022-02-22 07:28:18 -0600

    Fixing a bug in the comments, adding a check for assignee

--- template/gtd-detail-issue.go
+++ template/gtd-detail-issue.go
@@ -7,6 +7,7 @@ import (
 
 func GetOrgDetails(issue *jira.Issue) string {
 	var comments string
+	assignee := "Unassigned"
 
 	if issue.Fields.Comments != nil {
 		comments = fmt.Sprintf("\n#+begin_quote\n=========== COMMENTS ===========\n")
@@ -18,7 +19,11 @@ Author: %v
 Comment: %v
 -----`, c.Created, c.Author.DisplayName, c.Body)
 		}
-		comments = fmt.Sprintf("\n=========== COMMENTS ===========\n#+end_quote\n")
+		comments += fmt.Sprintf("\n========= END COMMENTS =========\n#+end_quote\n")
+	}
+
+	if issue.Fields.Assignee != nil {
+		assignee = issue.Fields.Assignee.DisplayName
 	}
 
 	text := fmt.Sprintf(`Issue: [[%v][%v]]
@@ -26,7 +31,7 @@ Description: %v
 Assigned To: %v
 Status: %v
 Comments: %v`, "Link", issue.Fields.Summary,
-		issue.Fields.Description, issue.Fields.Assignee.DisplayName,
+		issue.Fields.Description, assignee,
 		issue.Fields.Status.Name, comments)
 
 	return text
